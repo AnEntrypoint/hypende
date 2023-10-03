81a1ec7844160aeaf76a69b3483db6b179d6fdbb
lanmower <almagestfraternite@gmail.com>
2023-10-03 19:10:04 +0000

    jsdoc inclusion

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-require('dotenv').config()
-const startNode = require('./startNode.js')
-const crypto = require('hypercore-crypto')
-const b4a = require('b4a')
-const kp = crypto.keyPair(crypto.data(b4a.from(process.env.SEED)))
-console.log('PUBLICKEY FOR API '+kp.publicKey.toString('hex'))
-const run = async ()=>{
-    const nodes = ['task','fetch','test']
-    if(process.env.DISCORD_TOKEN) nodes.push('discord')
-    if(process.env.OPENAI_API_KEY) nodes.push('openai')
-    for(let name of nodes) {
-            startNode(kp, "hype", name)
-    }
-}
-run()
+require("dotenv").config();
+const startNode = require("./startNode.js");
+const crypto = require("hypercore-crypto");
+const b4a = require("b4a");
+
+const keyPair = crypto.keyPair(crypto.data(b4a.from(process.env.SEED)));
+console.log("PUBLICKEY FOR API " + keyPair.publicKey.toString("hex"));
+
+const run = async () => {
+  const nodes = ["task", "fetch", "test", "wikipedia", "google"];
+  if (process.env.DISCORD_TOKEN) {
+    nodes.push("discord");
+  }
+  if (process.env.OPENAI_API_KEY) {
+    nodes.push("openai");
+  }
+  for (let nodeName of nodes) {
+    startNode(keyPair, "hype", nodeName);
+  }
+};
+
+run();
